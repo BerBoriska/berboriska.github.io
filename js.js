95dd425879d3e56afb2853a60cac77805a10cc1c
BerBoriska <104057626+BerBoriska@users.noreply.github.com>
2022-06-19 11:37:34 +0300

    Адаптация
    Адаптировано под различные yстройства. Yбрано сообщение "Привет"

--- js.js
+++ js.js
@@ -1,3 +1,12 @@
 function show() {
 	document.querySelector('#kuz').style.display = "block";
+	document.querySelector('#kuz').style.background = "white";
+	
+}
+function order() {
+	document.querySelector('.order').style.top = "69px";
+
+	setTimeout(function() {
+		document.querySelector('.order').style.top = "-69px";
+	}, 5000);
 }
