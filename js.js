7fd1d01a154b02da5752a39b86280ec893cecaf5
BerBoriska <104057626+BerBoriska@users.noreply.github.com>
2024-09-28 15:55:18 +0300

    Second upload
    HTML and CSS adaptation

--- js.js
+++ js.js
@@ -2,6 +2,9 @@ let arrow = document.getElementsByClassName("category_left_item_arrow");
 let list_item = document.getElementsByClassName("category_left_item_list");
 let faq_arrow = document.getElementsByClassName("faq_item_arrow");
 let faq_list = document.getElementsByClassName("faq_item_list");
+let menuOpen = document.querySelector(".menu_open");
+let colse = document.querySelector(".close");
+let hat = document.querySelector(".hat");
 let light = 0;
 for (let i = 0;  i<arrow.length; i++) {
     arrow[i].addEventListener("click", function(){
@@ -19,3 +22,9 @@ for (let i = 0;  i < faq_arrow.length; i++) {
         faq_arrow[i].classList.toggle("faq_item_arrow_color");
     })
 }
+menuOpen.addEventListener("click", function(){
+    hat.style.top = "0%";
+})
+colse.addEventListener("click", function(){
+    hat.style.top = "-100%";
+})
