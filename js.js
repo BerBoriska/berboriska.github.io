94eefc4a86065892633e1371d72ce6c221aaf685
BerBoriska <104057626+BerBoriska@users.noreply.github.com>
2024-10-05 15:42:09 +0300

    third upload
    HTML+CSS+JS

--- js.js
+++ js.js
@@ -5,15 +5,13 @@ let faq_list = document.getElementsByClassName("faq_item_list");
 let menuOpen = document.querySelector(".menu_open");
 let colse = document.querySelector(".close");
 let hat = document.querySelector(".hat");
+let categLeft = document.querySelector(".category_left_fixed");
+let categOpen = document.querySelector(".categ_open");
+let categArrow = 0;
 let light = 0;
 for (let i = 0;  i<arrow.length; i++) {
     arrow[i].addEventListener("click", function(){
-    	/**/
-    	for (let j = 0;  j<arrow.length; j++) {
-    		list_item[j].classList.remove("category_left_item_list_active");
-    	}
-    	/**/
-        list_item[i].classList.add("category_left_item_list_active")
+    		list_item[i].classList.toggle("category_left_item_list_active");
     })
 }
 for (let i = 0;  i < faq_arrow.length; i++) {
@@ -28,3 +26,16 @@ menuOpen.addEventListener("click", function(){
 colse.addEventListener("click", function(){
     hat.style.top = "-100%";
 })
+
+categOpen.addEventListener("click", function(){
+    if (categArrow == 0) {
+            categLeft.style.left = "45px";
+            categArrow = 1;
+            categOpen.style.transform = "rotate(180deg)"
+    } else {
+            categLeft.style.left = "-200px";
+            categArrow = 0;
+            categOpen.style.transform = "rotate(0deg)"
+    }
+})
+
